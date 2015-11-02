dbcd3a35bf9a4b4a457f943d42f31cbf62a9d7bb
moisesiq <moises.deint@gmail.com>
2015-11-02 10:40:26 -0600

    - Reporte sql pagos a proveedores.

---
+++ ReportesSql/ProveedoresPagos.sql
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ * Reporte de pagos a proveedores
+ * Creado: 2015-11-02 Moisés
+ **************************************************************************** */
+
+SELECT
+	pp.FechaPago
+	, pv.NombreProveedor AS Proveedor
+	, mi.FolioFactura
+	, SUM(ppd.Importe) AS Importe
+FROM
+	ProveedorPoliza pp
+	LEFT JOIN Proveedor pv ON pv.ProveedorID = pp.ProveedorID AND pv.Estatus = 1
+	LEFT JOIN ProveedorPolizaDetalle ppd ON ppd.ProveedorPolizaID = pp.ProveedorPolizaID AND ppd.Estatus = 1
+	LEFT JOIN MovimientoInventario mi ON mi.MovimientoInventarioID = ppd.MovimientoInventarioID AND mi.Estatus = 1
+WHERE pp.Estatus = 1
+GROUP BY
+	pp.FechaPago
+	, pv.NombreProveedor
+	, mi.FolioFactura
+ORDER BY
+	pp.FechaPago
+	, mi.FolioFactura
