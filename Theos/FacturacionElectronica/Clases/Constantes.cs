0982d243dbaa4a09723ffd2c841fa4e7b8d2adfb
moisesiq <moises.deint@gmail.com>
2016-07-14 13:49:13 -0500

    - Facturación. Nueva modalidad en campo "metodoDePago".

---
+++ Theos/FacturacionElectronica/Clases/Constantes.cs
@@ -0,0 +1,15 @@
+﻿namespace FacturacionElectronica
+{
+    public static class CatFe
+    {
+        public static class MetodosDePago
+        {
+            public const string Efectivo = "01";
+            public const string Cheque = "02";
+            public const string Transferencia = "03";
+            public const string TarjetaDeCredito = "04";
+            public const string TarjetaDeDebito = "28";
+            public const string Otros = "99";
+        }
+    }
+}
