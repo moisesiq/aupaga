056a19d2a64defb0d6508240dc778c1c4ae5d4bf
moisesiq <moises.deint@gmail.com>
2016-07-21 19:20:26 -0500

    Faltantes.

---
+++ Theos/Refaccionaria.App/Ventas/Formas/FormasDePago.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Refaccionaria.App
+{
+    partial class FormasDePago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.lsbFormasDePago = new System.Windows.Forms.ListBox();
+            this.lsbSeleccionadas = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(63)))), ((int)(((byte)(87)))));
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(168, 126);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "&Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(63)))), ((int)(((byte)(87)))));
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(87, 126);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 12;
+            this.btnAceptar.Text = "&Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // lsbFormasDePago
+            // 
+            this.lsbFormasDePago.AllowDrop = true;
+            this.lsbFormasDePago.FormattingEnabled = true;
+            this.lsbFormasDePago.Location = new System.Drawing.Point(12, 12);
+            this.lsbFormasDePago.Name = "lsbFormasDePago";
+            this.lsbFormasDePago.Size = new System.Drawing.Size(150, 108);
+            this.lsbFormasDePago.TabIndex = 14;
+            this.lsbFormasDePago.DoubleClick += new System.EventHandler(this.lsbFormasDePago_DoubleClick);
+            // 
+            // lsbSeleccionadas
+            // 
+            this.lsbSeleccionadas.AllowDrop = true;
+            this.lsbSeleccionadas.FormattingEnabled = true;
+            this.lsbSeleccionadas.Location = new System.Drawing.Point(168, 12);
+            this.lsbSeleccionadas.Name = "lsbSeleccionadas";
+            this.lsbSeleccionadas.Size = new System.Drawing.Size(150, 108);
+            this.lsbSeleccionadas.TabIndex = 15;
+            this.lsbSeleccionadas.DoubleClick += new System.EventHandler(this.lsbSeleccionadas_DoubleClick);
+            // 
+            // FormasDePago
+            // 
+            this.AcceptButton = this.btnAceptar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(87)))), ((int)(((byte)(123)))));
+            this.ClientSize = new System.Drawing.Size(330, 160);
+            this.Controls.Add(this.lsbSeleccionadas);
+            this.Controls.Add(this.lsbFormasDePago);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormasDePago";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Selección de formas de pago";
+            this.Load += new System.EventHandler(this.FormasDePago_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.ListBox lsbFormasDePago;
+        private System.Windows.Forms.ListBox lsbSeleccionadas;
+    }
+}
---
+++ Theos/Refaccionaria.App/Ventas/Formas/FormasDePago.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using LibUtil;
+using TheosProc;
+
+namespace Refaccionaria.App
+{
+    public partial class FormasDePago : Form
+    {
+        private class FormaDePago
+        {
+            public int Id { get; set; }
+            public string Forma { get; set; }
+            public override string ToString()
+            {
+                return this.Forma;
+            }
+        }
+
+        public FormasDePago()
+        {
+            InitializeComponent();
+        }
+
+        #region [ Propiedades ]
+
+        public List<VentasPagosDetalleView> FormasDePagoSel { get; set; }
+
+        #endregion
+
+        #region [ Eventos ]
+
+        private void FormasDePago_Load(object sender, EventArgs e)
+        {
+            // Se llenan las formas de pago
+            this.LlenarListas();
+        }
+
+        private void lsbFormasDePago_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.lsbFormasDePago.Focused)
+                this.PasarElemento(this.lsbFormasDePago, this.lsbSeleccionadas, this.lsbFormasDePago.SelectedItem);
+        }
+
+        private void lsbSeleccionadas_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.lsbSeleccionadas.Focused)
+                this.PasarElemento(this.lsbSeleccionadas, this.lsbFormasDePago, this.lsbSeleccionadas.SelectedItem);
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            this.LlenarFormasDePago();
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        #endregion
+
+        #region [ Privados ]
+
+        private void PasarElemento(ListBox oListaDe, ListBox oListaA, object oElemento)
+        {
+            if (oElemento != null)
+            {
+                oListaA.Items.Add(oElemento);
+                oListaDe.Items.Remove(oElemento);
+            }
+        }
+
+        private bool ExisteElemento(ListBox oLista, object oElemento)
+        {
+            var oForma = (oElemento as FormaDePago);
+            int iFormaID = oForma.Id;
+            foreach (object oReg in oLista.Items)
+            {
+                var oFormaReg = (oReg as FormaDePago);
+                if (oFormaReg.Id == iFormaID)
+                    return true;
+            }
+            return false;
+        }
+
+        private void LlenarFormasDePago()
+        {
+            if (this.FormasDePagoSel == null)
+                this.FormasDePagoSel = new List<VentasPagosDetalleView>();
+            this.FormasDePagoSel.Clear();
+            foreach (object oReg in this.lsbSeleccionadas.Items)
+            {
+                var oFormaReg = (oReg as FormaDePago);
+                this.FormasDePagoSel.Add(new VentasPagosDetalleView() { FormaDePagoID = oFormaReg.Id, FormaDePago = oFormaReg.Forma });
+            }
+        }
+
+        #endregion
+
+        #region [ Públicos ]
+
+        public void LlenarListas()
+        {
+            var oFormas = Datos.GetListOf<TipoFormaPago>(c => c.Estatus);
+            this.lsbFormasDePago.Items.Clear();
+            this.lsbSeleccionadas.Items.Clear();
+            foreach (var oReg in oFormas)
+                this.lsbFormasDePago.Items.Add(new FormaDePago { Id = oReg.TipoFormaPagoID, Forma = oReg.NombreTipoFormaPago });
+
+            if (this.FormasDePagoSel == null)
+                return;
+
+            foreach (var oReg in this.FormasDePagoSel)
+            {
+                for (int i = 0; i < this.lsbFormasDePago.Items.Count; i++)
+                {
+                    var oForma = this.lsbFormasDePago.Items[i];
+                    var oFormaReg = (oForma as FormaDePago);
+                    if (oFormaReg.Id == oReg.FormaDePagoID)
+                    {
+                        this.lsbSeleccionadas.Items.Add(oForma);
+                        this.lsbFormasDePago.Items.RemoveAt(i--);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
