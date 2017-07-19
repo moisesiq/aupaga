93e319c0483ef74a97d52e33fe81e5276e75add5
Atanacio <atanacio.exe@gmail.com>
2017-07-19 18:40:32 -0500

    ventas resx

--- Theos/Refaccionaria.App/Formas/ValidarUsuario.Designer.cs
+++ Theos/Refaccionaria.App/Formas/ValidarUsuario.Designer.cs
@@ -84,6 +84,7 @@
             this.cmbSucursal.Name = "cmbSucursal";
             this.cmbSucursal.Size = new System.Drawing.Size(160, 21);
             this.cmbSucursal.TabIndex = 0;
+            //this.cmbSucursal.SelectedIndexChanged += new System.EventHandler(this.cmbSucursal_SelectedIndexChanged);
             // 
             // txtContrasenia
             // 
